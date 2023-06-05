bda1396e3fbfbe002d7796262d6329176378acda
robohen <88636103+robohen@users.noreply.github.com>
2023-06-05 14:00:02 -0700

    Person class complete with methods

--- lab.js
+++ lab.js
@@ -14,3 +14,46 @@ class Hamster {
         console.log(`${this.price}`);
     }
   }
+  class Person {
+    constructor(name) {
+      this.name = name;
+      this.age = 0;
+      this.height = 0;
+      this.weight = 0;
+      this.mood = 0;
+      this.hamsters = [];
+      this.bankAccount = 0;
+    }
+    getName(){
+        console.log(`${this.name}`);
+    }
+    getAge(){
+        console.log(`${this.age}`);
+    }
+    getWeight(){
+        console.log(`${this.weight}`);
+    }
+    getHeight(){
+        console.log(`${this.height}`);
+    }
+    greet(){
+        console.log(`Hello I am ${this.name}`);
+    }
+    eat(){
+        weight++, mood++;
+    }
+    exercise(){
+        this.weight--;
+    }
+    ageUp(){
+        this.age++;
+        this.height++;
+        this.weight++;
+        this.mood--;
+        this.bankAccount+=10;
+    }
+    buyHamster(hamster){
+        hamster.push();
+    }
+
+  }
